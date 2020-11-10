96ae0c08c0001bc6252fbf16684cb93de37b2c75
gammelalf <31348226+gammelalf@users.noreply.github.com>
2020-11-10 05:46:35 +0100

    Made dice operator support unary

--- extensions/dice.py
+++ extensions/dice.py
@@ -11,5 +11,8 @@ class Dice(Operator):
     def priority(self):
         return 100
 
-    def __call__(self, x, y):
+    def binary(self, x, y):
         return sum((randint(1, y) for i in range(x)))
+
+    def unary(self, x):
+        return randint(1, x)
